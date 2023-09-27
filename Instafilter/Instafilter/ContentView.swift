95af1027450da3cba8899780c6f4e11fca1b4f14
= <=>
2023-09-27 08:50:55 +0200

    Added a nice looking color scheme for the UI, as well as an Image synbol. No added functionality for this version.

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -24,6 +24,19 @@ struct ContentView: View {
     
     @State private var processedImage: UIImage?
     
+    let color1 = Color(red: 0.0312, green: 0.2969, blue: 0.3789)
+    let color2 = Color(red: 0.8555, green: 0.3125, blue: 0.2891)
+    let color3 = Color(red: 0.8867, green: 0.7070, blue: 0.0195)
+    
+    // titleColor is the same as color3
+    let titleColor = UIColor(red: 0.8867, green: 0.7070, blue: 0.0195, alpha: 1)
+    
+    // this initializer is to be able to set the color of the navigationTitle
+    init() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: titleColor]
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: titleColor]
+    }
+    
     
     // body
     var body: some View {
@@ -32,10 +45,17 @@ struct ContentView: View {
                 ZStack {
                     Rectangle()
                         .fill(.secondary)
+                        .opacity(0.7)
                     
-                    Text("Tap to select a picture")
-                        .foregroundColor(.white)
-                        .font(.headline)
+                    VStack{
+                        Text("Tap to select a picture")
+                            .foregroundColor(color2)
+                            .font(.headline)
+                        Image(systemName: "photo")
+                            .font(.system(size: 70.0))
+                            .foregroundColor(.red)
+                            .padding([.top], 2)
+                    }
                     
                     image?
                         .resizable()
@@ -48,7 +68,9 @@ struct ContentView: View {
                 
                 HStack {
                     Text("Intensity")
+                        .foregroundColor(color3)
                     Slider(value: $filterIntensity)
+                        .accentColor(color2)
                         .onChange(of: filterIntensity) { _ in
                             applyProcessing()
                         }
@@ -60,17 +82,24 @@ struct ContentView: View {
                         // change the filter
                         showingFilterSheet = true
                     }
+                    .foregroundColor(color3)
                     
                     Spacer()
                     
                     // save the picture
                     Button("Save", action: save)
+                        .foregroundColor(color3)
                 }
             }
             .padding([.horizontal, .bottom])
-            .navigationTitle("Instafilter")
+            
+            // navigation title
+            .navigationTitle(Text("Instafilter"))
+            //.foregroundColor(color2)
+            
             .sheet(isPresented: $showingImagePicker) {
                 ImagePicker(image: $inputImage)
+                    .ignoresSafeArea()
             }
             .onChange(of: inputImage) { _ in loadImage() }
             .confirmationDialog("Select a filter", isPresented: $showingFilterSheet) {
@@ -84,6 +113,7 @@ struct ContentView: View {
                 Button("Vignette") { setFilter(CIFilter.vignette()) }
                 Button("Cancel", role: .cancel) { }
             }
+            .background(color1)
             
         } // closing brace of the NavigationView
     } // closing brace of the body
